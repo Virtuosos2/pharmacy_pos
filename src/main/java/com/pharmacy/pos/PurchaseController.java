a80de6a612ab5b359f419754917523c46ae05a68
pooja sharma <poojasharma8928@gmail.com>
2023-08-11 16:02:04 +0530

    Post changes

--- src/main/java/com/pharmacy/pos/PurchaseController.java
+++ src/main/java/com/pharmacy/pos/PurchaseController.java
@@ -6,27 +6,38 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PurchaseController {
-	
+
 	@Autowired
 	private PurchaseService purchaseService;
-	
+
 	@GetMapping("/purchase")
 	public List<Purchase> PurchaseDetails() {
 		return purchaseService.getAllPurchase();
-		
-	} 
-	@RequestMapping("/purchase/{id}")	
-		public Purchase getPurchase(@PathVariable String  id){
-			return purchaseService.getPurchase(id);
-		}
-		
+
 	}
+
+	@RequestMapping("/purchase/{id}")
+	public Purchase getPurchase(@PathVariable String id) {
+		return purchaseService.getPurchase(id);
+	}
+
 	
-
-
-
+	 @PostMapping("/purchase")
+	 public void addProduct(@RequestBody Purchase product) { 
+     purchaseService.addProduct(product);
+	  
+	  }
+	 
+	 @PostMapping("/purchase/{id}")
+	 public void updateProduct(@RequestBody Purchase product, @PathVariable String id) {
+		 purchaseService.updateProduct(id , product);
+	 }
+	 
+}
--- src/main/java/com/pharmacy/pos/PurchaseService.java
+++ src/main/java/com/pharmacy/pos/PurchaseService.java
@@ -1,5 +1,6 @@
 package com.pharmacy.pos;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,14 +10,14 @@ import org.springframework.stereotype.Service;
 public class PurchaseService {
 
 	
-	private List<Purchase> purchase = Arrays.asList(
+	private List<Purchase> purchase = new ArrayList<>( Arrays.asList(
 			new Purchase("101" , "Paracetomol" , 10 , 4, 40),
 			new Purchase("102" , "Dolo 365" , 15, 2, 30),
 			new Purchase("103" , "Cheston cold" , 20 , 3, 60),
 			new Purchase("104" , "Jelosine" , 8 , 2, 16),
 			new Purchase("105" , "Azithromycin" , 25 , 2, 50)
 			
-			);
+			));
 	
 	public List<Purchase> getAllPurchase(){
 		return purchase;
@@ -26,7 +27,21 @@ public class PurchaseService {
     public Purchase getPurchase(String id){
     	return purchase.stream().filter(n -> n.getPurchase_id().equals(id)).findFirst().get();
     	
+    }	
+    
+    public void addProduct(Purchase product) {
+    	purchase.add(product);
+    	
     }
-		
-	
+    
+    public void updateProduct(String id , Purchase product) {
+    	for(int i=0;i< purchase.size();i++) {
+    		Purchase p = purchase.get(i);
+    		if(p.getPurchase_id().equals(id)) {
+    			purchase.set(i, p);
+    			return;
+    		}
+  		
+    	}
+    }
 }
